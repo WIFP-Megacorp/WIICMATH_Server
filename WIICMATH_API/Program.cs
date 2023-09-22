93a805602b7e4a347e1194ee62b626208f3abfa0
Trivinyx <tom.a.s.myre@gmail.com>
2023-09-22 04:25:01 +0200

    APIclassLib intergartion
    Beginngi of intergration

--- WIICMATH_API/Program.cs
+++ WIICMATH_API/Program.cs
@@ -1,3 +1,4 @@
+using APIclassLib;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -9,16 +10,29 @@ var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 
-	app.UseSwagger();
-	app.UseSwaggerUI();
+app.UseSwagger();
+app.UseSwaggerUI();
 
 
 app.UseHttpsRedirection();
 
+//Get the default settings for the device from the APIclassLib
+app.MapGet(pattern: "Api/Device/Settings", () => { return WIICMATHdevice.DefaultSettings; });
+
+//gets the device settings for the device with the given id
+//not implemented yet
+//app.MapGet(pattern: "Api/Device/Settings/{id}", (object id) => { return "Not implemented yet"; });
+
+//endpoint for device to post data readings
+app.MapPost(pattern: "Api/Device/Readings", (WIICMATHdevice device) => { return device; });
+
+
+
+
 //example
 //app.MapGet("/weatherforecast", () =>
 //{
-	
+
 //})
 //.WithName("GetWeatherForecast");
 
