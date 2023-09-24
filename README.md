2e36bac9aa92afbc469f6012265884bf764dcf0d
Trivinyx <tom.a.s.myre@gmail.com>
2023-09-24 21:43:50 +0200

    #2 Completed the remaining endpoint
    All currently avalable methodes in IDeviceData and IDeviceLogData are endpointed. closes #2
    
    Updated README.md with a guide on how to run the projeckt in VS.

--- README.md
+++ README.md
@@ -10,3 +10,28 @@ These YouTube videos are a great resource for learning how to use this solution 
 
 Understading classLibrary in general:
 - [ClassLibrary](https://www.youtube.com/watch?v=C6LV_xMGdKc)
+
+
+How to make the api project work locally(Visual Studio):
+
+1. Open the solution in Visual Studio.
+2. right-click the WIICMATH_API, make sure it is set as startup projeckt
+3. right-click the WIICMATH_API, click publish. In the popup:
+	- click edit
+	- click browse
+	- select local
+	- select MYSQLLocalDB
+	- click ok
+	- name the db
+	- save profile as in the projekt folder (easyer to publish later)
+	- dobble click the new publish file
+	- click publish
+4.Search "SQL server Object Explorer".
+5.Navigate til you find your named DB
+6.Right-click the DB and click properties, find the connetion string.	
+7.Open the WIICMATH_API project, open the appsettings.json file.
+8.Replace the connection string with the one you found in step 6.
+	- The connection string should look something like this: "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=WIICMATH_DB;Integrated Security=True;Connect Timeout=60;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False"
+	- Make sure there are no spaces in (TrustServerCertificate ,ApplicationIntent and MultiSubnetFailover)	
+9. Save the appsettings.json
+1. Run the project
--- WIICMATH_API/Api.cs
+++ WIICMATH_API/Api.cs
@@ -5,9 +5,20 @@ public static class Api
 	public static void ConfigureAPI(this WebApplication app)
 	{
 		//All of my Api endpoint mapping
+		//Device endpoints
 		app.MapGet("/api/devices", GetDevices); //Get all devices
+		app.MapGet("/api/devices/{ArdMac}", GetDevice); //Get a device by its ArdMac
+		app.MapPost("/api/insertDevice", InsertDevice);
+		app.MapPost("/api/deleteDevice", DeleteDevice);
+		//DeviceLog endpoints
+		app.MapGet("/api/deviceLogs", GetDeviceLogs); //Get all device logs
+		app.MapGet("/api/deviceLogs/{ArdMac}", GetLogsByDevice); //Get all device logs by device ArdMac
+		app.MapPost("/api/insertDeviceLog", InsertDeviceLog);
+		app.MapPost("/api/deleteDeviceLog", DeleteDeviceLog);
 	}
-	
+
+	//IDeviceData endpoints methodes
+	//getAll
 	private static async Task<IResult> GetDevices(IDeviceData data)
 	{
 		try
@@ -16,7 +27,110 @@ public static class Api
 		}
 		catch (Exception ex)
 		{
-			return Results.Problem(ex.Message);	
+			return Results.Problem(ex.Message);
+		}
+	}
+	//get with string parameter
+	private static async Task<IResult> GetDevice(IDeviceData data, string ArdMac)
+	{
+		try
+		{
+			return Results.Ok(await data.GetDevice(ArdMac));
+		}
+		catch (Exception ex)
+		{
+			return Results.Problem(ex.Message);
+		}
+	}
+	//Insert
+	private static async Task<IResult> InsertDevice(DeviceModel device, IDeviceData data)
+	{
+		try
+		{
+			await data.InsertDevice(device);
+			return Results.Ok();
+		}
+		catch (Exception ex)
+		{
+			return Results.Problem(ex.Message);
+		}
+	}
+	//Delete
+	private static async Task<IResult> DeleteDevice(int? id, string? ArdMac, IDeviceData data)
+	{
+		try
+		{
+			await data.DeleteDevice(id, ArdMac);
+			return Results.Ok();
+		}
+		catch (Exception ex)
+		{
+			return Results.Problem(ex.Message);
+		}
+	}
+	//Update
+	private static async Task<IResult> UpdateDevice(DeviceModel device, IDeviceData data)
+	{
+		try
+		{
+			await data.UpdateDevice(device);
+			return Results.Ok();
+		}
+		catch (Exception ex)
+		{
+			return Results.Problem(ex.Message);
+		}
+	}
+
+	//IDeviceLogData endpoints methodes
+	//getAll
+	private static async Task<IResult> GetDeviceLogs(IDeviceLogData data)
+	{
+		try
+		{
+			return Results.Ok(await data.GetDeviceLogs());
+		}
+		catch (Exception ex)
+		{
+			return Results.Problem(ex.Message);
+		}
+	}
+	//get device spesific logs
+	private static async Task<IResult> GetLogsByDevice(IDeviceLogData data, string ArdMac)
+	{
+		try
+		{
+			return Results.Ok(await data.GetByDevice(ArdMac));
+		}
+		catch (Exception ex)
+		{
+			return Results.Problem(ex.Message);
+		}
+	}
+	//Insert
+	private static async Task<IResult> InsertDeviceLog(DeviceLogModel log, IDeviceLogData data)
+	{
+		try
+		{
+			await data.Insert(log);
+			return Results.Ok();
+		}
+		catch (Exception ex)
+		{
+			return Results.Problem(ex.Message);
+		}
+	}
+	//Delete
+	private static async Task<IResult> DeleteDeviceLog(int id, IDeviceLogData data)
+	{
+		try
+		{
+			await data.Delete(id);
+			return Results.Ok();
+		}
+		catch (Exception ex)
+		{
+			return Results.Problem(ex.Message);
 		}
 	}
 }
