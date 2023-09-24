6e1614c1a7e382a08e1a089d3699a7d0ff7aac59
Trivinyx <tom.a.s.myre@gmail.com>
2023-09-24 07:17:55 +0200

    Added first endpoint

--- WIICMATH_API/Api.cs
+++ WIICMATH_API/Api.cs
@@ -5,7 +5,18 @@ public static class Api
 	public static void ConfigureAPI(this WebApplication app)
 	{
 		//All of my Api endpoint mapping
-		
+		app.MapGet("/api/devices", GetDevices); //Get all devices
 	}
 	
+	private static async Task<IResult> GetDevices(IDeviceData data)
+	{
+		try
+		{
+			return Results.Ok(await data.GetDevices());
+		}
+		catch (Exception ex)
+		{
+			return Results.Problem(ex.Message);	
+		}
+	}
 }
