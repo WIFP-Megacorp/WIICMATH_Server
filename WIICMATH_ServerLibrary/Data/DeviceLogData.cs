726bf5e907c533c4e523562a77a713a585dc4bb4
Trivinyx <tom.a.s.myre@gmail.com>
2023-09-24 06:11:24 +0200

    Added logic
    Added logic for calling db with correct dbo.sp's
    removed some usings
    created an interface for class

--- WIICMATH_ServerLibrary/Data/DeviceLogData.cs
+++ WIICMATH_ServerLibrary/Data/DeviceLogData.cs
@@ -1,13 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using WIICMATH_ServerLibrary.DbAccess;
+﻿using WIICMATH_ServerLibrary.DbAccess;
+using WIICMATH_ServerLibrary.Models;
 
 namespace WIICMATH_ServerLibrary.Data;
 
-public class DeviceLogData
+public class DeviceLogData : IDeviceLogData
 {
 	private readonly ISqlDataAccess _db;
 
@@ -15,4 +11,21 @@ public class DeviceLogData
 	{
 		_db = db;
 	}
+
+	public Task<IEnumerable<DeviceLogModel>> GetDeviceLogs() => _db.LoadData<DeviceLogModel, dynamic>("dbo.spDeviceLog_GetAll", new { });
+
+	//private get methode to get deviceID from ArdMac
+	private async Task<int> getDeviceId(string ardmac)
+	{
+		var result = (await _db.LoadData<DeviceModel, dynamic>("dbo.spDevice_Get", new { ArdMAC = ardmac })).FirstOrDefault();
+		if (result == null) return -1;
+		return result.Id;
+	}
+
+	//custom get with param ArdMac
+	public Task<IEnumerable<DeviceLogModel>> GetByDevice(string ArdMac) => _db.LoadData<DeviceLogModel, dynamic>("dbo.spDeviceLog_GetDevice", new { DeviceID = getDeviceId(ArdMac) });
+
+	public Task Insert(DeviceLogModel log) => _db.SaveData("dbo.spDeviceLog_Insert", new { log.DeviceId, log.Temperature, log.Humidity, log.TimeStamp });
+
+	public Task Delete(int id) => _db.SaveData("dbo.spDeviceLog_Delete", new { Id = id });
 }
---
+++ WIICMATH_ServerLibrary/Data/IDeviceLogData.cs
@@ -0,0 +1,11 @@
+﻿using WIICMATH_ServerLibrary.Models;
+
+namespace WIICMATH_ServerLibrary.Data;
+
+public interface IDeviceLogData
+{
+	Task Delete(int id);
+	Task<IEnumerable<DeviceLogModel>> GetByDevice(string ArdMac);
+	Task<IEnumerable<DeviceLogModel>> GetDeviceLogs();
+	Task Insert(DeviceLogModel log);
+}
