2c636c28117fbb7ce4b52dc48345d254fb35ed7e
Tom André Slåen Myre <tom.a.s.myre@gmail.com>
2023-10-02 15:27:05 +0200

    Adding docker compose (#20)
    Docker Compose added
    - makes 2 contaiers: API and DB
    
    The API and DB has configuration of connection in compose
    added a dockerfile to build an SQL server image, customise it and unpacking the database ".dacpac" file. This i located in DBservice directory.
    
    Made changes to API and Server lib to accomondate some of the new changes with docker compose.
    
    
    solving #18

--- WIICMATH_API/Program.cs
+++ WIICMATH_API/Program.cs
@@ -7,7 +7,20 @@ var builder = WebApplication.CreateBuilder(args);
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddSingleton<ISqlDataAccess, SqlDataAccess>();
+
+//get parts of connection string from Evnironment variable if it exists
+//check if the environment variable exists
+if (Environment.GetEnvironmentVariable("DB_HOST") != null && Environment.GetEnvironmentVariable("DB_NAME") != null && Environment.GetEnvironmentVariable("DB_PASS") !=null) { 
+	var dbHost = Environment.GetEnvironmentVariable("DB_HOST");
+	var dbName = Environment.GetEnvironmentVariable("DB_NAME");
+	var dbPass = Environment.GetEnvironmentVariable("DB_PASS");
+	//creating the string
+	var connectionString = $"Data Source={dbHost};Initial Catalog={dbName};User Id=sa;Password={dbPass};";
+	//builds the service with the connection string
+	builder.Services.AddSingleton<ISqlDataAccess>(new SqlDataAccess(connectionString, builder.Configuration));
+	
+} else { builder.Services.AddSingleton<ISqlDataAccess, SqlDataAccess>(); }
+
 builder.Services.AddSingleton<IDeviceData, DeviceData>();
 builder.Services.AddSingleton<IDeviceLogData, DeviceLogData>();
 
--- WIICMATH_ServerLibrary/DbAccess/SqlDataAccess.cs
+++ WIICMATH_ServerLibrary/DbAccess/SqlDataAccess.cs
@@ -8,15 +8,25 @@ namespace WIICMATH_ServerLibrary.DbAccess;
 public class SqlDataAccess : ISqlDataAccess
 {
 	private readonly IConfiguration _config;
+	private string? connectionString;
 
 	public SqlDataAccess(IConfiguration config)
 	{
 		_config = config;
 	}
-	//Get
-	public async Task<IEnumerable<T>> LoadData<T, U>(string storedProcedure, U parameters, string connectionId = "Default")
+	public SqlDataAccess(string connectionString, IConfiguration config)
 	{
-		using IDbConnection connection = new SqlConnection(_config.GetConnectionString(connectionId));
+		this.connectionString = connectionString;
+		_config = config;
+    }
+    //Get
+    public async Task<IEnumerable<T>> LoadData<T, U>(string storedProcedure, U parameters, string connectionId = "Default")
+	{
+		if (connectionString == null)
+		{
+			connectionString = _config.GetConnectionString(connectionId);
+		}
+		using IDbConnection connection = new SqlConnection(connectionString);
 
 		return await connection.QueryAsync<T>(storedProcedure, parameters, commandType: CommandType.StoredProcedure);
 	}
@@ -24,7 +34,11 @@ public class SqlDataAccess : ISqlDataAccess
 	//insert, update, delete
 	public async Task SaveData<T>(string storedProcedure, T parameter, string connectionId = "Default")
 	{
-		using IDbConnection connection = new SqlConnection(_config.GetConnectionString(connectionId));
+		if (connectionString == null)
+		{
+			connectionString = _config.GetConnectionString(connectionId);
+		}
+		using IDbConnection connection = new SqlConnection(connectionString);
 
 		await connection.ExecuteAsync(storedProcedure, parameter, commandType: CommandType.StoredProcedure);
 	}
